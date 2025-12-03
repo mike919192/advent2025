0205a023a38dc1a8332f1675609e82b1568fe23b
Mike Bloom <mike.r.bloom@protonmail.com>
2025-12-03 00:18:02 +0000

    Day2 part 2 working

--- day2/day2.cpp
+++ day2/day2.cpp
@@ -12,13 +12,12 @@ using pair_t = std::pair<long, long>;
 std::vector<pair_t> read_file()
 {
     std::ifstream infile("input.txt");
-    std::vector<pair_t> rotations;
+    std::vector<pair_t> pairs;
 
     for (std::string line; std::getline(infile, line);) {
         std::istringstream ss(line);
 
-        while(true)
-        {
+        while (true) {
             long num1 = 0;
             long num2 = 0;
             char dash = 0;
@@ -27,23 +26,22 @@ std::vector<pair_t> read_file()
             if (!(ss >> num1 >> dash >> num2))
                 break;
 
-            rotations.push_back(pair_t(num1, num2));
+            pairs.push_back(pair_t(num1, num2));
 
             if (!(ss >> comma))
                 break;
         }
     }
 
-    return rotations;
+    return pairs;
 }
 
-int main()
+long part1(const std::vector<pair_t> &pairs)
 {
-    const auto pairs = read_file();
     std::regex repeat_regex("(^\\d+)+\\1$");
-    long sum_part1{0};
+    long sum{ 0 };
 
-    for (const auto & pair : pairs) {
+    for (const auto &pair : pairs) {
         for (long num = pair.first; num <= pair.second; num++) {
             auto check_str = std::to_string(num);
             auto check_begin = std::sregex_iterator(check_str.begin(), check_str.end(), repeat_regex);
@@ -54,11 +52,46 @@ int main()
                 std::string match_str = match.str();
 
                 if (match.size() > 0) {
-                    sum_part1 += num;
+                    sum += num;
                 }
             }
         }
     }
 
+    return sum;
+}
+
+long part2(const std::vector<pair_t> &pairs)
+{
+    std::regex repeat_regex("(^\\d+)+\\1{1,}$");
+    long sum{ 0 };
+
+    for (const auto &pair : pairs) {
+        for (long num = pair.first; num <= pair.second; num++) {
+            auto check_str = std::to_string(num);
+            auto check_begin = std::sregex_iterator(check_str.begin(), check_str.end(), repeat_regex);
+            auto check_end = std::sregex_iterator();
+
+            for (std::sregex_iterator i = check_begin; i != check_end; ++i) {
+                std::smatch match = *i;
+                std::string match_str = match.str();
+
+                if (match.size() > 0) {
+                    sum += num;
+                }
+            }
+        }
+    }
+
+    return sum;
+}
+
+int main()
+{
+    const auto pairs = read_file();
+
+    long sum_part1 = part1(pairs);
     std::cout << sum_part1 << '\n';
+    long sum_part2 = part2(pairs);    
+    std::cout << sum_part2 << '\n';
 }
