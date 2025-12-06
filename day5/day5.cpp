c402fba4dab9f2a6d779274f47af75e574362a16
Mike Bloom <mike.r.bloom@protonmail.com>
2025-12-06 01:05:39 +0000

    Day 5 part 2 working

--- day5/day5.cpp
+++ day5/day5.cpp
@@ -43,6 +43,23 @@ std::tuple<std::vector<pair_t>, std::vector<long>> read_file()
     return { id_ranges, ids };
 }
 
+//this algorithm requires the ranges to be sorted
+void part2_merge_ranges(std::vector<pair_t> &id_ranges)
+{
+    for (size_t i = 0; i < id_ranges.size() - 1; i++) {
+        for (size_t j = i + 1; j < id_ranges.size(); j++) {
+            //if (i) second is greater than (j) first than we can merge
+            if (id_ranges.at(i).second >= id_ranges.at(j).first - 1) {
+                id_ranges.at(i).second = std::max(id_ranges.at(i).second, id_ranges.at(j).second);
+                id_ranges.erase(id_ranges.begin() + j);
+                j--;
+            } else {
+                break;
+            }
+        }
+    }
+}
+
 int main()
 {
     const auto [id_ranges, ids] = read_file();
@@ -56,5 +73,16 @@ int main()
             part1_count++;
     }
 
+    auto id_ranges_mut = id_ranges;
+
+    std::ranges::sort(id_ranges_mut);
+    part2_merge_ranges(id_ranges_mut);
+
+    long part2_count{ 0 };
+
+    for (const auto &range : id_ranges_mut)
+        part2_count += range.second - range.first + 1;
+
     std::cout << part1_count << '\n';
+    std::cout << part2_count << '\n';
 }
